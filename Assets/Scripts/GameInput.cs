bb17361b68c79db1f1448e785e75119380c6ce34
Kartikay Sharma <sharma.kartik2107@gmail.com>
2024-01-06 16:17:55 +0530

    extraced user input to separate script

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour {
+    public Vector2 GetNormalisedMovementVector() {
+        Vector2 inputVector = new(0,0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x += -1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.y += -1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
     private void Update() {
-        Vector2 inputVector = new(0,0);
-
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x += -1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y += -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x += 1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetNormalisedMovementVector();
 
         Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
         transform.position += moveSpeed * Time.deltaTime * moveDir;
