b8ff4c28d81af96b060d1b031385d29baf92fdb1
Kartikay Sharma <sharma.kartik2107@gmail.com>
2024-01-09 23:30:30 +0530

    added player collison with physics capsule cast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed;
     [SerializeField] private GameInput gameInput;
     private bool isWalking;
     private void Update() {
         Vector2 inputVector = gameInput.GetNormalisedMovementVector();
 
         Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
-        transform.position += moveSpeed * Time.deltaTime * moveDir;
+
+        float playerHeight = 2f;
+        float playerRadius = 0.7f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canNotMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (canNotMove) {
+            // can not move in moveDir
+
+            // attempt moving in x direction
+            Vector3 moveDirX = new(moveDir.x, 0, 0);
+            bool canNotMoveX = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canNotMoveX) {
+                // attempt moving in Z direction
+                Vector3 moveDirZ = new(0, 0, moveDir.z);
+                bool canNotMoveZ = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canNotMoveZ) {
+                    // can not move in any direction
+                }
+                else {
+                    // move in Z
+                    transform.position += moveDistance * moveDirZ.normalized;
+                }
+            }
+            else {
+                // move in X
+                transform.position += moveDistance * moveDirX.normalized;
+            }
+        }
+        else {
+            transform.position += moveDistance * moveDir;
+        }
 
         isWalking = moveDir != Vector3.zero;
          
